154f8548b3d9ce8763e41bf93ca4694553f5fa5b
emadyasin <emad.yasin823@gmail.com>
2023-08-10 12:01:49 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-
-
 /**
- * main - 101-qoute.c
- * description - Write a C program that prints exactly and that piece of
- * art is useful\"
- *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * main - main function
  *
- * Return: Always 0 (Success)
+ * Return: 1
  */
+
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }

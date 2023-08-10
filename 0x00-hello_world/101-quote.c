96d4ddfc17a5f7a7d4b242dcc438f3e020898014
emadyasin <emad.yasin823@gmail.com>
2023-08-10 11:19:15 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
 
 /**
- * main - this is main fuction
+ * Entry point
  * Return: Always 1 (success)
  */
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }

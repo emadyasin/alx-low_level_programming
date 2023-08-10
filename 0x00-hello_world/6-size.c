ba810057d25942468fd2acca192357ad0fdc88a4
emadyasin <emad.yasin823@gmail.com>
2023-08-10 14:47:20 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 
-	return 0;
+	return (0);
 }

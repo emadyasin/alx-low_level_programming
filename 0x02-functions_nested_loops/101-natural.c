05aec376dcc550ccd96ad1bec3ceff56f1415c92
emadyasin <emad.yasin823@gmail.com>
2023-08-15 14:58:15 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- * followed by a new line
- * Return: Always 0
+ * main - prints the sum of all fibonacci numbers less than 1024
+ * Return: alwyas 0 (success)
  */
 int main(void)
 {
-	long int i, j, k, next;
+	int sum = 0;
 
-	j = 1;
-
-	k = 2;
-
-	for (i = 1; i <= 50; i++)
+	for (int i = 0; i < 1024; i++)
 	{
-		if (j != 20365011074)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf("%ld, ", j);
-		} else
-		{
-			printf("%ld\n", j);
+			sum += i;
 		}
-		next = j + k;
-		j = k;
-		k = next;
 	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }

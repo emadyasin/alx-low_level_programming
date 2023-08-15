282f7f37d643bed90f54a4123a1cf54c56c82147
emadyasin <emad.yasin823@gmail.com>
2023-08-15 14:10:24 -0700

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - print all ntural numbers from n to 98
- * @n: the starting number
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: starting number
+ * Return: nothing
  */
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n < 98; n++)
-			printf("%d; ", n);
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	}
 	else
-	{
-		for (; n > 98; n--)
-			printf("%d; ", n);
-	}
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
 	printf("98\n");
 }

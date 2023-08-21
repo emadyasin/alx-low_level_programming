dd21271ab015709f0477641889b5ab70c578774a
emadyasin <emad.yasin823@gmail.com>
2023-08-21 11:17:20 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,36 +2,37 @@
 
 /**
  * _atoi - converts a sring to an integer
- * @s: input string
- * Return: integer
+ * @s: string to be converted
+ * Return: the in converted from the string
  */
-
 int _atoi(char *s)
 {
-	unsigned int  count = 0, size = 0, pn = 1, oi = 0, m = 1, i;
+	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
 
-	while (*(s + count) != '\0')
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
+		if (s[i] == '_')
+			++d;
 
-		if (*(s + count) == '_')
-			pn *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (size > 0)
-				m *= 10;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
 
-			size++;
+			f = 0;
 		}
-		count++;
-	}
+		i++;
 
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
 	}
-	return (oi * pn);
+	if (f == 0)
+		return (0);
+
+	return (n);
 }

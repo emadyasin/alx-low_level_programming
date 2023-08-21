459940e0b729cbad73ff4098282b140207624e74
emadyasin <emad.yasin823@gmail.com>
2023-08-21 11:20:42 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,15 +5,24 @@
  * @s: string to be converted
  * Return: the in converted from the string
  */
+
 int _atoi(char *s)
 {
-	int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
-		if (s[i] == '_')
+		if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
@@ -25,12 +34,11 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-
 			f = 0;
 		}
 		i++;
-
 	}
+
 	if (f == 0)
 		return (0);
 
